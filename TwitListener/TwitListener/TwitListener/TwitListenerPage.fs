29a9156b5ab13bf1c832b78fcff5b7903d696d9f
Rafael Lorandi <coolparadox@gmail.com>
2017-02-25 12:27:34 -0300

    C# Portable Library replaced by F# version

---
+++ TwitListener/TwitListener/TwitListener/TwitListenerPage.fs
@@ -0,0 +1,18 @@
+﻿namespace TwitListener
+
+open System
+open Xamarin.Forms
+
+type TwitListenerPage() = 
+    inherit ContentPage()
+
+    do base.Content <- Label(Text = "Hello, F# Xamarin.Forms",
+                             HorizontalOptions = LayoutOptions.Center,
+                             VerticalOptions = LayoutOptions.Center,
+                             FontSize = 24.0,
+                             TextColor = Color.Pink,
+                             FontAttributes = FontAttributes.Italic,
+                             Rotation = -15.0)
+
+type App() = 
+    inherit Application(MainPage = TwitListenerPage())
