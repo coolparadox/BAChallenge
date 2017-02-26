d76f573dc660ccc24f73fc21547fd865cdc3e19e
Rafael Lorandi <coolparadox@gmail.com>
2017-02-26 00:50:34 -0300

    Raw list view sample

--- TwitListener/TwitListener/TwitListener/TwitListenerPage.fs
+++ TwitListener/TwitListener/TwitListener/TwitListenerPage.fs
@@ -4,9 +4,22 @@ open System
 open Xamarin.Forms
 open Xamarin.Forms.Xaml
 
+type TweetsViewItem = {
+    Who : string
+    When : DateTime
+    What : string
+}
+ 
 type TwitListenerPage() = 
     inherit ContentPage()
     let _ = base.LoadFromXaml(typeof<TwitListenerPage>)
+    let tweetsView = base.FindByName<ListView>("tweetsView")
+    let tweetsViewItems = [
+        { Who = "coolparadox"; When = DateTime.Now; What = "tweet this dude" }
+        { Who = "starcrusher"; When = DateTime.Now; What = "naah I use messenger duh" }
+    ]
+    do tweetsView.ItemsSource <- ((tweetsViewItems |> List.toSeq) :> Collections.IEnumerable)
+
 
 type App() = 
     inherit Application(MainPage = TwitListenerPage())
