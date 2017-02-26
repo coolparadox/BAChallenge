434d63abded2d2e15983503880a9ccc6786ff0ef
Rafael Lorandi <coolparadox@gmail.com>
2017-02-26 01:38:57 -0300

    Multiline text in ListView

--- TwitListener/TwitListener/TwitListener/TwitListenerPage.fs
+++ TwitListener/TwitListener/TwitListener/TwitListenerPage.fs
@@ -15,11 +15,30 @@ type TwitListenerPage() =
     let _ = base.LoadFromXaml(typeof<TwitListenerPage>)
     let tweetsView = base.FindByName<ListView>("tweetsView")
     let tweetsViewItems = [
-        { Who = "coolparadox"; When = DateTime.Now; What = "tweet this dude" }
-        { Who = "starcrusher"; When = DateTime.Now; What = "naah I use messenger duh" }
+        { Who = "@coolparadox"; When = DateTime.Now; What = "tweet this dude #partiu" }
+        { Who = "@starcrusher"; When = DateTime.Now; What = "naah I use messenger duh! Twitter SMS verification conde is soooooo slow it never reaches destiny argh" }
+        { Who = "@coolparadox"; When = DateTime.Now; What = "tweet this dude #partiu" }
+        { Who = "@starcrusher"; When = DateTime.Now; What = "naah I use messenger duh! Twitter SMS verification conde is soooooo slow it never reaches destiny argh" }
+        { Who = "@coolparadox"; When = DateTime.Now; What = "tweet this dude #partiu" }
+        { Who = "@starcrusher"; When = DateTime.Now; What = "naah I use messenger duh! Twitter SMS verification conde is soooooo slow it never reaches destiny argh" }
+        { Who = "@coolparadox"; When = DateTime.Now; What = "tweet this dude #partiu" }
+        { Who = "@starcrusher"; When = DateTime.Now; What = "naah I use messenger duh! Twitter SMS verification conde is soooooo slow it never reaches destiny argh" }
+        { Who = "@coolparadox"; When = DateTime.Now; What = "tweet this dude #partiu" }
+        { Who = "@starcrusher"; When = DateTime.Now; What = "naah I use messenger duh! Twitter SMS verification conde is soooooo slow it never reaches destiny argh" }
+        { Who = "@coolparadox"; When = DateTime.Now; What = "tweet this dude #partiu" }
+        { Who = "@starcrusher"; When = DateTime.Now; What = "naah I use messenger duh! Twitter SMS verification conde is soooooo slow it never reaches destiny argh" }
+        { Who = "@coolparadox"; When = DateTime.Now; What = "tweet this dude #partiu" }
+        { Who = "@starcrusher"; When = DateTime.Now; What = "naah I use messenger duh! Twitter SMS verification conde is soooooo slow it never reaches destiny argh" }
+        { Who = "@coolparadox"; When = DateTime.Now; What = "tweet this dude #partiu" }
+        { Who = "@starcrusher"; When = DateTime.Now; What = "naah I use messenger duh! Twitter SMS verification conde is soooooo slow it never reaches destiny argh" }
+        { Who = "@coolparadox"; When = DateTime.Now; What = "tweet this dude #partiu" }
+        { Who = "@starcrusher"; When = DateTime.Now; What = "naah I use messenger duh! Twitter SMS verification conde is soooooo slow it never reaches destiny argh" }
+        { Who = "@coolparadox"; When = DateTime.Now; What = "tweet this dude #partiu" }
+        { Who = "@starcrusher"; When = DateTime.Now; What = "naah I use messenger duh! Twitter SMS verification conde is soooooo slow it never reaches destiny argh" }
+        { Who = "@coolparadox"; When = DateTime.Now; What = "tweet this dude #partiu" }
+        { Who = "@starcrusher"; When = DateTime.Now; What = "naah I use messenger duh! Twitter SMS verification conde is soooooo slow it never reaches destiny argh" }
     ]
     do tweetsView.ItemsSource <- ((tweetsViewItems |> List.toSeq) :> Collections.IEnumerable)
-
 
 type App() = 
     inherit Application(MainPage = TwitListenerPage())
