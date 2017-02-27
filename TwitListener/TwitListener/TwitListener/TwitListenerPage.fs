2b500daab7822680c7a49faa2accaeb280be0edb
Rafael Lorandi <coolparadox@gmail.com>
2017-02-27 18:57:47 -0300

    Added toolbar with About option.
    Side note: navigating documentation and inferring code for enablement of toolbar in Xamarin Forms / F# was hardcore... Nice to have it enabled once it's working, but it took way longer that I expected.

--- TwitListener/TwitListener/TwitListener/TwitListenerPage.fs
+++ TwitListener/TwitListener/TwitListener/TwitListenerPage.fs
@@ -21,14 +21,29 @@ type TwitListenerPage() =
             fun _ arg -> tweetsView.ItemsSource <- ((arg |> List.toSeq) :> Collections.IEnumerable)
         )
 
+    member this.OnAboutClicked() =
+        let message = "This is a simple Twitter stream API exerciser by coolparadox@gmail.com"
+        this.DisplayAlert ("Twitter Listener", message, "OK") |> ignore
+
+    override this.OnAppearing() =
+        base.OnAppearing()
+        this.ToolbarItems.Clear()
+        this.ToolbarItems.Add(ToolbarItem("About", "", (fun _ -> this.OnAboutClicked()), ToolbarItemOrder.Default, 0))
+
     member this.OnButtonClicked(sender : Object, args : EventArgs) = 
         modelManager.addTweet({Who="@coolparadox"; When=DateTime.Now; What="the quick brown fox jumps over the lazy dog"})
 
 type App() = 
 
-    inherit Application(MainPage = TwitListenerPage())
+    inherit Application(MainPage = NavigationPage(TwitListenerPage()))
 
     override this.OnStart() =
         base.OnStart()
-        // Subscribe to changes in domain model.
-        (this.MainPage :?> TwitListenerPage).SubscribeToModelUpdates
+
+        // Disable navigation bar.
+        let navigationPage = this.MainPage :?> NavigationPage
+        NavigationPage.SetHasNavigationBar(navigationPage, false)
+
+        // Subscribe content page to events from domain model.
+        let twitListenerPage = navigationPage.CurrentPage :?> TwitListenerPage
+        twitListenerPage.SubscribeToModelUpdates
