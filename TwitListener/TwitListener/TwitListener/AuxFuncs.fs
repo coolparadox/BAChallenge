834a16967a4b06a585e887420919ff0a8637317b
Rafael Lorandi <coolparadox@gmail.com>
2017-02-27 16:20:43 -0300

    Workaround for MissingMethodException error for List.take in Android deployment

---
+++ TwitListener/TwitListener/TwitListener/AuxFuncs.fs
@@ -0,0 +1,10 @@
+﻿module AuxFuncs
+
+// Workaround for List.take error:
+// "System.MissingMethodException: Method Microsoft.FSharp.Collections not found"
+let rec take n list =
+    match (n, list) with
+    | (_, []) -> []
+    | (0, _) -> []
+    | (n, x::xs) -> x :: take (n-1) xs
+
--- TwitListener/TwitListener/TwitListener/ModelManager.fs
+++ TwitListener/TwitListener/TwitListener/ModelManager.fs
@@ -14,6 +14,6 @@ type ModelManager private () =
     // Add a new tweet to the Model and notify listeners.
     member this.addTweet(tweet:ModelTypes.Tweet) =
         // Prepend new tweet to the list, protecting against infinite growth.
-        Model.tweets <- tweet :: List.take 500 Model.tweets
+        Model.tweets <- tweet :: AuxFuncs.take 500 Model.tweets
         // Notify subscribers that tweet list was changed.
         MessagingCenter.Send<ModelManager, ModelTypes.Tweet list> (this, "onTweetListChanged", Model.tweets);
